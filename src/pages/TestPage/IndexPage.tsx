a7681494cfa9dc4a2f4fb2114de67a368926ae91
ribbinpo <ribbinpo@gmail.com>
2025-02-08 17:31:47 +0700

    fix incorrect link

--- src/pages/TestPage/IndexPage.tsx
+++ src/pages/TestPage/IndexPage.tsx
@@ -12,17 +12,17 @@ export const IndexPage: FC = () => {
           header="Application Launch Data"
           footer="These pages help developer to learn more about current launch information"
         >
-          <Link to="/init-data">
+          <Link to="/test/init-data">
             <Cell subtitle="User data, chat information, technical data">
               Init Data
             </Cell>
           </Link>
-          <Link to="/launch-params">
+          <Link to="/test/launch-params">
             <Cell subtitle="Platform identifier, Mini Apps version, etc.">
               Launch Parameters
             </Cell>
           </Link>
-          <Link to="/theme-params">
+          <Link to="/test/theme-params">
             <Cell subtitle="Telegram application palette information">
               Theme Parameters
             </Cell>
