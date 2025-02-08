6820efcaf6a7f0ee47a497137fe13bf191f928e0
ribbinpo <ribbinpo@gmail.com>
2025-02-08 11:50:30 +0700

    feat: :sparkles: add telegram

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { Root } from '@/components/Root.tsx';
+import { EnvUnsupported } from '@/components/EnvUnsupported.tsx';
+import { init } from '@/init.ts';
+
+import '@telegram-apps/telegram-ui/dist/styles.css';
+import './index.css';
+
+// Mock the environment in case, we are outside Telegram.
+import './mockEnv.ts';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+try {
+  // Configure all application dependencies.
+  init(retrieveLaunchParams().startParam === 'debug' || import.meta.env.DEV);
+
+  root.render(
+    <StrictMode>
+      <Root/>
+    </StrictMode>,
+  );
+} catch (e) {
+  console.error(e);
+  root.render(<EnvUnsupported/>);
+}
